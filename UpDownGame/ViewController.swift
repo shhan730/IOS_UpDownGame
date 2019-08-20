2d053947e27ce77902607c94d50d432c88be90d4
Seung Hun Han <shhan730@gmail.com>
2019-08-21 05:47:24 +0900

    Initial Commit

---
+++ UpDownGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UpDownGame
+//
+//  Created by HSH on 21/08/2019.
+//  Copyright © 2019 Ajou University D2fault. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
