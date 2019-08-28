ceab0fe8ba40630b61801ca3e9fc9175d8f4d0b9
Seung Hun Han <shhan730@gmail.com>
2019-08-28 21:39:56 +0900

    Set Main StoryBoard

--- UpDownGame/ViewController.swift
+++ UpDownGame/ViewController.swift
@@ -9,7 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //Mark:- Properties
+    
+    //Mark: IBOutlets
+    @IBOutlet weak var resultLable: UILabel!
+    @IBOutlet weak var turnCountLable: UILabel!
+    @IBOutlet weak var inputField: UITextField!
+    
+    @IBAction func touchUpSubmitButton(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func touchUpResetButton(_ sender: UIButton) {
+        
+    }
+    @IBAction func tapBackground(_ sender: UITapGestureRecognizer) {
+       
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
